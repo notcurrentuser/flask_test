e5f4eecacc529d4f49199765168390b27ae3105c
notcurrentuser <notcurrentuser@gmail.com>
2022-10-29 22:18:31 +0300

    edit: get product api, add review api

--- app/api.py
+++ app/api.py
@@ -13,53 +13,63 @@ load_dotenv()
 @app.route('/products/<product_id>', methods=['GET'])
 def get_product(product_id):
     if request.method == 'GET':
-        product = redis_db.get(product_id)
+        try:
+            product = redis_db.get(product_id)
 
-        PAGE_ELEMENTS_NUMBER = 2
-        pages_number = int(request.args.get('page', 1))
-        start_page_elements = (pages_number - 1) * PAGE_ELEMENTS_NUMBER
-        finish_page_elements = pages_number * PAGE_ELEMENTS_NUMBER
+            PAGE_ELEMENTS_NUMBER = 2
+            pages_number = int(request.args.get('page', 1))
+            start_page_elements = (pages_number - 1) * PAGE_ELEMENTS_NUMBER
+            finish_page_elements = pages_number * PAGE_ELEMENTS_NUMBER
 
-        if not product:
-            product = models.Product.query.filter_by(id=product_id).first()
             if not product:
-                abort(404, 'Product not found')
-            product = product.to_dict()
+                product = models.Product.query.filter_by(id=product_id).first()
+                if not product:
+                    abort(404, 'Product not found')
+                product = product.to_dict()
 
-            product_reviews = models.Review.query.filter_by(asin=product['Asin'])
-            product['Reviews'] = [product_review.to_dict() for product_review in product_reviews]
+                product_reviews = models.Review.query.filter_by(asin=product['Asin'])
+                product['Reviews'] = [product_review.to_dict() for product_review in product_reviews]
 
-            redis_db.set(product['id'], json.dumps(product).encode('utf-8'), ex=int(os.getenv("REDIS_TTL")))
-        else:
-            product = json.loads(product)
+                redis_db.set(product['id'], json.dumps(product).encode('utf-8'), ex=int(os.getenv("REDIS_TTL")))
+            else:
+                product = json.loads(product)
 
-        product['Reviews'] = product['Reviews'][start_page_elements: finish_page_elements]
+            product['Reviews'] = product['Reviews'][start_page_elements: finish_page_elements]
 
-        return product
+            return product
+
+        except Exception as e:
+            print(e)
+            abort(404)
 
 
 @app.route('/add_review', methods=['PUT'])
 def add_review():
     if request.method == 'PUT':
-        request_data = request.get_json()
+        try:
+            request_data = request.get_json()
 
-        product_id = request_data['Product ID']
+            product_id = request_data['Product ID']
 
-        title = request_data['Title']
-        review = request_data['Review']
+            title = request_data['Title']
+            review = request_data['Review']
 
-        product = models.Product.query.filter_by(id=product_id).first()
+            product = models.Product.query.filter_by(id=product_id).first()
 
-        if not product:
-            abort(404, f'Product {product_id} not found')
+            if not product:
+                abort(404, f'Product {product_id} not found')
 
-        product = product.to_dict()
+            product = product.to_dict()
 
-        new_review = models.Review(asin=product['Asin'], title=title, review=review)
-        db.session.add(new_review)
-        db.session.commit()
+            new_review = models.Review(asin=product['Asin'], title=title, review=review)
+            db.session.add(new_review)
+            db.session.commit()
 
-        abort(200)
+            abort(200)
+
+        except Exception as e:
+            print(e)
+            abort(404)
 
 
 def start_api():
