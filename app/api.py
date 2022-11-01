f87ea17f3e854e1c780230fd555ebef9791b0a7e
notcurrentuser <notcurrentuser@gmail.com>
2022-11-01 15:25:28 +0200

    add comments in api.py

--- app/api.py
+++ app/api.py
@@ -14,22 +14,24 @@ load_dotenv()
 def get_product(product_id):
     if request.method == 'GET':
         try:
-            product = redis_db.get(product_id)
+            product = redis_db.get(product_id)  # get redis cache ('None' if there is no cache)
 
-            PAGE_ELEMENTS_NUMBER = 2
-            pages_number = int(request.args.get('page', 1))
-            start_page_elements = (pages_number - 1) * PAGE_ELEMENTS_NUMBER
-            finish_page_elements = pages_number * PAGE_ELEMENTS_NUMBER
+            PAGE_ELEMENTS_NUMBER = 2  # the number of elements on the page
+            pages_number = int(request.args.get('page', 1))  # get the page from the '?page=' attribute
+            start_page_elements = (pages_number - 1) * PAGE_ELEMENTS_NUMBER  # the first element on the page
+            finish_page_elements = pages_number * PAGE_ELEMENTS_NUMBER  # the last element on the page
 
-            if not product:
-                product = models.Product.query.filter_by(id=product_id).first()
+            if not product:  # if there is no cache
+                product = models.Product.query.filter_by(id=product_id).first()  # get the product from the db
                 if not product:
-                    abort(404, 'Product not found')
+                    abort(404, 'Product not found')  # if the product not found, then we return 404
                 product = product.to_dict()
 
-                product_reviews = models.Review.query.filter_by(asin=product['Asin'])
+                product_reviews = models.Review.query.filter_by(asin=product['Asin'])  # get the reviews from the db
+                # convert review from class elements to a dictionary
                 product['Reviews'] = [product_review.to_dict() for product_review in product_reviews]
 
+                # save cache
                 redis_db.set(product['id'], json.dumps(product).encode('utf-8'), ex=int(os.getenv("REDIS_TTL")))
             else:
                 product = json.loads(product)
@@ -47,6 +49,7 @@ def get_product(product_id):
 def add_review():
     if request.method == 'PUT':
         try:
+            # get data from put-request
             request_data = request.get_json()
 
             product_id = request_data['Product ID']
@@ -54,13 +57,15 @@ def add_review():
             title = request_data['Title']
             review = request_data['Review']
 
+            # get product
             product = models.Product.query.filter_by(id=product_id).first()
 
             if not product:
-                abort(404, f'Product {product_id} not found')
+                abort(404, f'Product {product_id} not found')  # if the product not found, then we return 404
 
             product = product.to_dict()
 
+            # save review in db
             new_review = models.Review(asin=product['Asin'], title=title, review=review)
             db.session.add(new_review)
             db.session.commit()
